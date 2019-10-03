88a6690a6c2f6732804dc7801957c7f4380f9d42
NikitaBIZ <piven_2001@mail.ru>
2019-10-03 23:07:45 +0300

    Правка

--- Laba1/Laba1/Program.cs
+++ Laba1/Laba1/Program.cs
@@ -13,29 +13,31 @@ namespace Laba1
             //y = Convert.ToDouble(Console.ReadLine());
             //Console.WriteLine("Введи z");
             //z = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Введи p");
-            p = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Введи p");
+            //p = Convert.ToDouble(Console.ReadLine());
             x = 182.5;
             y = 18.225;
             z = -0.03298;
             e = Math.Exp(1);
             double f = fd(x, y, z);
             Console.WriteLine(f);
-           
             
-                if (x > Math.Abs(p)) l = (2 * Math.Pow(e, x)); 
-                if (3 < x)
-                {
-                  if (x<Math.Abs(p)) l = Math.Abs(Math.Pow(e, x) - p);
 
-                }
-                if (x == Math.Abs(p)) l = Math.Pow(Math.Pow(e, x) - p, 2);
-            
+
+
+            //if (x > Math.Abs(p)) l = (2 * Math.Pow(e, x)); 
+            //if (3 < x)
+            //{
+            //  if (x<Math.Abs(p)) l = Math.Abs(Math.Pow(e, x) - p);
+
+            //}
+            //if (x == Math.Abs(p)) l = Math.Pow(Math.Pow(e, x) - p, 2);
+
         }
         static double fd (double x, double y, double z)
         {
             double f;
-            f = Math.Abs(Math.Pow(x, y / x) - Math.Pow(y / x, 1 / 3)) + (y - x) * ((Math.Cos(y) - (z / (y - x))) / (1 + Math.Pow(y - x, 2)));
+            f = Math.Abs(Math.Pow(x, y / x) - Math.Pow(y / x, 1.0 / 3.0)) + (y - x) * ((Math.Cos(y) - (z / (y - x))) / (1 + Math.Pow(y - x, 2)));
             return f;
            
         }
